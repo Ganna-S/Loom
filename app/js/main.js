6a5f0f5e778a3f08d6cf46551af72843374052af
Ganna-S <gannashutenko@gmail.com>
2019-12-26 23:36:47 +0200

    added slider

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $('.slider__box').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prew"><img src="../images/slide-left.png" alt=""></button>',
+        nextArrow: false,
+    });
+
+});
