72571e97094af1afbc1c14cf153523a536868d75
Ganna-S <gannashutenko@gmail.com>
2020-01-03 00:07:17 +0200

    added customers, journal, footer

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,12 @@ $(function () {
         prevArrow: '<button class="slick-arrow slick-prew"><img src="../images/slide-left.png" alt=""></button>',
         nextArrow: false,
     });
+    $('.customers__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: false,
+        arrows: false,
+    });
 
 });
